c1cf74769b95a7ba06252a95b1d5a8988693d8ac
Anshu-Mane <anshumane.official08@gmail.com>
2026-01-12 18:49:19 +0000

    Gmap and live location

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import { signup, login } from "./firebase.js";
+import { onAuthStateChanged } from "firebase/auth";
+import { collection, addDoc } from "firebase/firestore";
 
 function signupUser() {
   signup(
@@ -14,4 +16,38 @@ function signupUser() {
 
 function loginUser() {
   login(email.value, password.value);
+}
+
+let currentLat = null;
+let currentLng = null;
+/*Google Map and Live location */
+function startLocation() {
+  if (!navigator.geolocation) {
+    alert("Geolocation not supported");
+    return;
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    (pos) => {
+      currentLat = pos.coords.latitude;
+      currentLng = pos.coords.longitude;
+
+      const map = new google.maps.Map(
+        document.getElementById("map"),
+        {
+          zoom: 15,
+          center: { lat: currentLat, lng: currentLng }
+        }
+      );
+
+      new google.maps.Marker({
+        position: { lat: currentLat, lng: currentLng },
+        map: map,
+        title: "You are here"
+      });
+    },
+    () => {
+      alert("Location access denied");
+    }
+  );
 }
